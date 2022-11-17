b95153b25b2b577428346e0d0dabc3ce02df65ca
Abhishek5520 <abhishekkumar552002@gmail.com>
2022-11-17 20:45:39 +0530

    Firebase Login

--- app/src/main/java/com/example/e_book/DashBoardAdminActivity.kt
+++ app/src/main/java/com/example/e_book/DashBoardAdminActivity.kt
@@ -1,11 +1,39 @@
 package com.example.e_book
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.e_book.databinding.ActivityDashBoardAdminBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class DashBoardAdminActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashBoardAdminBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dash_board_admin)
+        binding = ActivityDashBoardAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        binding.logoutBtn.setOnClickListener {
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+        else{
+            val email = firebaseUser.email
+            binding.subTitleTv.text = email
+        }
     }
 }
--- app/src/main/java/com/example/e_book/DashBoardUserActivity.kt
+++ app/src/main/java/com/example/e_book/DashBoardUserActivity.kt
@@ -1,11 +1,39 @@
 package com.example.e_book
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.e_book.databinding.ActivityDashBoardUserBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class DashBoardUserActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashBoardUserBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dash_board_user)
+        binding = ActivityDashBoardUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        binding.logoutBtn.setOnClickListener {
+            firebaseAuth.signOut()
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null){
+            binding.subTitleTv.text = "Not Logged In"
+        }
+        else{
+            val email = firebaseUser.email
+            binding.subTitleTv.text = email
+        }
     }
 }
