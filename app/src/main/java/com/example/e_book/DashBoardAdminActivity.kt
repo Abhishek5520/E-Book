4c44572c6bc7b44e4a4a31dd6e2c7993d9aab9fc
Abhishek5520 <abhishekkumar552002@gmail.com>
2022-11-23 09:53:19 +0530

    Show Book Categories

--- app/src/main/java/com/example/e_book/DashBoardAdminActivity.kt
+++ app/src/main/java/com/example/e_book/DashBoardAdminActivity.kt
@@ -3,15 +3,27 @@ package com.example.e_book
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import com.example.e_book.databinding.ActivityDashBoardAdminBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.lang.Exception
 
 class DashBoardAdminActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDashBoardAdminBinding
 
     private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+
+    private lateinit var adapterCategory: AdapterCategory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashBoardAdminBinding.inflate(layoutInflater)
@@ -19,6 +31,26 @@ class DashBoardAdminActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+        loadCategories()
+
+        binding.searchEt.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                try {
+                    adapterCategory.filter.filter(s)
+                }
+                catch (e: Exception){
+
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
 
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
@@ -29,6 +61,30 @@ class DashBoardAdminActivity : AppCompatActivity() {
             startActivity(Intent(this, CategoryAddActivity::class.java))
             Animatoo.animateSlideLeft(this)
         }
+    }
+
+    private fun loadCategories() {
+
+        categoryArrayList = ArrayList()
+
+        val ref  = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                categoryArrayList.clear()
+                for (ds in snapshot.children){
+                    val model = ds.getValue(ModelCategory::class.java)
+
+                    categoryArrayList.add(model!!)
+                }
+
+                adapterCategory = AdapterCategory(this@DashBoardAdminActivity,categoryArrayList)
+                binding.categoriesRv.adapter = adapterCategory
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
     private fun checkUser() {
